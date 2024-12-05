69e63ebddc87518ec33883813df9b90bb6a1af52
Gabriele Lucarelli <l-gabri1998@live.it>
2024-12-05 15:39:37 +0100

    try

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,28 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+import Welcome from "./components/Welcome";
+import BookList from "./components/BookList";
+import book from "./data/fantasy.json";
 
-test('renders learn react link', () => {
+test("renders learn react link", () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+describe("Welcome Component", () => {
+  test("renders Welcome component with the correct heading text", () => {
+    render(<Welcome />);
+    const headingElement = screen.getByRole("heading", { level: 1 });
+    expect(headingElement).toBeInTheDocument();
+    expect(headingElement).toHaveTextContent("Benvenuti in EpiBooks!");
+  });
+});
+
+describe("BookList", () => {
+  it("dovrebbe renderizzare tante carte quanti sono i libri", () => {
+    render(<BookList books={book} />);
+    const cards = screen.getAllByTestId("single-book");
+    expect(cards.length).toBe(book.length);
+  });
 });
